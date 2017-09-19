df4cf5e36020d66cf03bb465e08c7a5fff98b135
yintaishan <yintaishan@gmail.com>
2017-09-19 11:59:52 +0800

    ￥￥￥￥commit￥￥￥￥

--- app/src/main/java/com/au/gitdemoaaa/MainActivity.java
+++ app/src/main/java/com/au/gitdemoaaa/MainActivity.java
@@ -10,15 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // bugfix01  fix bug
-        //dfsdfdfasdfsdf
-        //dfsdfdfasdfsdf
-
-        //asldfaskjdflsd
-        //lsdfkjslkdjf
-
-        //sdlkfjlskadjflksdjf
-        //slkfdslakdfjlskdjf
 
 
     }
