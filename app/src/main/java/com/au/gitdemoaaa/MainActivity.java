25295ee780f68acc464e3c78d6e1bf80556680a4
yintaishan <yintaishan@gmail.com>
2017-09-19 11:07:25 +0800

    // bugfix01 分支  修改内容01

--- app/src/main/java/com/au/gitdemoaaa/MainActivity.java
+++ app/src/main/java/com/au/gitdemoaaa/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // bugfix01 分支  修改内容01
     }
 }
